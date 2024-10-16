e517b2dd667b9b2e8d9f85c4df295f4f63918949
wade.huang <wade.huang@tcci.com.tw>
2024-10-16 15:50:51 +0800

    with shared memory

--- src/matrixMul_share.cu
+++ src/matrixMul_share.cu
@@ -1,63 +1,61 @@
-// CUDA runtime
 #include <cuda_runtime.h>
 #include "matmul.h"
 #include <iostream>
 #define BLOCK_SIZE 32
 
-/**
- * This code is from https://docs.nvidia.com/cuda/cuda-c-programming-guide/index.html#cuda-c-runtime
- * Figure 8 Matrix Multiplication without Shared Memory
- */
-
-// width = column, height = row
-
-// Forward declaration of the matrix multiplication kernel
 // Matrix multiplication kernel called by MatMul()
-__global__ void MatMulKernel(Matrix A, Matrix B, Matrix C)
+__global__ void matrixMultiplyShared(Matrix A, Matrix B, Matrix C)
 {
-    // Each thread reads one row of A and one column of B and computes the corresponding element of C
+
     float Cvalue = 0;
+    // Thread row and column within Csub
     int row = blockIdx.y * blockDim.y + threadIdx.y;
     int col = blockIdx.x * blockDim.x + threadIdx.x;
-    for (int k = 0; k < A.column; k++)
-        Cvalue += A.elements[row * A.column + k] * B.elements[k * B.column + col];
-    C.elements[row * C.column + col] = Cvalue;
+
+    // Shared memory used to store Asub and Bsub respectively
+    __shared__ float As[BLOCK_SIZE][BLOCK_SIZE];
+    __shared__ float Bs[BLOCK_SIZE][BLOCK_SIZE];
+
+    for (int m = 0; m < (A.column / BLOCK_SIZE); ++m)
+    {
+        As[threadIdx.y][threadIdx.x] = A.elements[(blockIdx.y * BLOCK_SIZE + threadIdx.y) * A.column + BLOCK_SIZE * m + threadIdx.x];
+        Bs[threadIdx.y][threadIdx.x] = B.elements[(m * BLOCK_SIZE + threadIdx.y) * B.column + blockIdx.x * BLOCK_SIZE + threadIdx.x];
+
+        // Synchronize to make sure the sub-matrices are loaded
+        // before starting the computation
+        __syncthreads();
+        // Multiply Asub and Bsub together
+        for (int e = 0; e < BLOCK_SIZE; ++e)
+            Cvalue += As[row][e] * Bs[e][col];
+        // Synchronize to make sure that the preceding
+        // computation is done before loading two new
+        // sub-matrices of A and B in the next iteration
+        __syncthreads();
+    }
+    // Write Csub to device memory
+    // Each thread writes one element
+    C.elements[row * B.column + col] = Cvalue;
 }
 
 namespace matmul
 {
-    // Matrix multiplication - Host code
-    // Matrix dimensions are assumed to be multiples of BLOCK_SIZE
-    void MatmulOperator::mat_mul_cuda(const Matrix &A, const Matrix &B, Matrix &C)
+    void MatmulOperator::mat_mul_cuda_shared(const Matrix &A, const Matrix &B, Matrix &C)
     {
-
-        for (int i = 0; i < C.row; i++)
-            for (int j = 0; j < C.column; j++)
-                C.elements[i * C.column + j] = 0;
-
         // Load A and B to device memory
         Matrix d_A;
         d_A.column = A.column;
         d_A.row = A.row;
         size_t size = A.column * A.row * sizeof(float);
-
-        // Allocate memory
         cudaMalloc(&d_A.elements, size);
-
-        // Copy data to GPU
         cudaMemcpy(d_A.elements, A.elements, size,
                    cudaMemcpyHostToDevice);
         Matrix d_B;
         d_B.column = B.column;
         d_B.row = B.row;
         size = B.column * B.row * sizeof(float);
-
-        // Allocate memory
         cudaMalloc(&d_B.elements, size);
-        // Copy data to GPU
         cudaMemcpy(d_B.elements, B.elements, size,
                    cudaMemcpyHostToDevice);
-
         // Allocate C in device memory
         Matrix d_C;
         d_C.column = C.column;
@@ -68,13 +66,10 @@ namespace matmul
         // Invoke kernel
         dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
         dim3 dimGrid(C.column / dimBlock.x, C.row / dimBlock.y);
-        std::cout << "Computing result using CUDA Kernel...\n";
-        MatMulKernel<<<dimGrid, dimBlock>>>(d_A, d_B, d_C);
-
+        matrixMultiplyShared<<<dimGrid, dimBlock>>>(d_A, d_B, d_C);
         // Read C from device memory
         cudaMemcpy(C.elements, d_C.elements, size,
                    cudaMemcpyDeviceToHost);
-
         // Free device memory
         cudaFree(d_A.elements);
         cudaFree(d_B.elements);
